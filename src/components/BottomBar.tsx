4dc05a5fc280b40ab3ad55f0fea89e96bf63cb84
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2024-12-07 20:58:19 +0900

    feat: 하단바 아이콘 및 타이틀 제작. 버튼 추가

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BottomNavBar, ItemButton, ItemIcon } from '@/styles/componenets/Style_BottomBar';
+import { BottomNavBar, ItemButton, ItemIcon, HomeButton, MainIcon, ItemTitle } from '@/styles/componenets/Style_BottomBar';
 import { useNavigate } from 'react-router-dom';
 
 const BottomBar: React.FC = () => {
@@ -9,18 +9,30 @@ const BottomBar: React.FC = () => {
         <BottomNavBar>
             <ItemButton onClick={ () => navigate('/')}>
                 <ItemIcon src="/src/resources/icons/social_menu_icon.svg" />
-            </ItemButton>
-            <ItemButton onClick={ () => navigate('/friends/{:id}')}>
-                <ItemIcon src="/src/resources/icons/friends_menu_icon.svg" />
+                <ItemTitle> 커뮤니티 </ItemTitle>
             </ItemButton>
             <ItemButton onClick={ () => navigate('/')}>
-                <ItemIcon src="/src/resources/icons/home_menu_icon.svg" />
+                <ItemIcon src="/src/resources/icons/video_menu_icon.svg" />
+                <ItemTitle> 동영상 </ItemTitle>
+            </ItemButton>
+            <ItemButton onClick={ () => navigate('/')}>
+                <ItemIcon src="/src/resources/icons/view_menu_icon.svg" />
+                <ItemTitle> 뷰 </ItemTitle>
+            </ItemButton>
+            <HomeButton onClick={ () => navigate('/')}>
+                <MainIcon src="/src/resources/icons/home_menu_icon.svg" />
+            </HomeButton>
+            <ItemButton onClick={ () => navigate('/friends/{:id}')}>
+                <ItemIcon src="/src/resources/icons/friends_menu_icon.svg" />
+                <ItemTitle> 친구 </ItemTitle>
             </ItemButton>
             <ItemButton onClick={ () => navigate('/chatlist/{:id}')}>
                 <ItemIcon src="/src/resources/icons/chat_menu_icon.svg" />
+                <ItemTitle> 메세지 </ItemTitle>
             </ItemButton>
             <ItemButton onClick={ () => navigate('/mypage/{:id}')}>
                 <ItemIcon src="/src/resources/icons/profile_menu_icon.svg" />
+                <ItemTitle> 마이페이지 </ItemTitle>
             </ItemButton>
         </BottomNavBar>
     );
--- src/styles/componenets/Style_BottomBar.ts
+++ src/styles/componenets/Style_BottomBar.ts
@@ -14,10 +14,18 @@ export const BottomNavBar = styled.nav`
 `;
 
 export const ItemIcon = styled.img`
-    width: 35px;
+    width: 30px;
+`
+export const ItemTitle = styled.a`
+    color: ${colors.mainColor_1};
+    font-size: 15px;
+    font-weight: bold;
+`
+export const MainIcon = styled.img`
+    width: 40px;
 `
 
-export const ItemButton = styled.div`
+export const HomeButton = styled.div`
     background-color: ${colors.mainColor_2};
     color: white;
     width: 100%;
@@ -26,7 +34,23 @@ export const ItemButton = styled.div`
     align-items: center;
     display: flex;
     &:hover {
-        opacity: 0.5;
+        opacity: 0.7;
+        cursor: pointer;
+    }
+`
+
+export const ItemButton = styled.div`
+    background-color: ${colors.mainColor_2};
+    color: white;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    gap: 5px;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    &:hover {
+        opacity: 0.7;
         cursor: pointer;
     }
 `
